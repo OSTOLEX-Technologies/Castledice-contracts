f7b16735b0f4edb4fc8e3357fa05deafb3d56e4f
1KitCat1 <denischool119@gmail.com>
2023-10-06 01:20:58 +0300

    Add buy item

--- contracts/Marketplace.sol
+++ contracts/Marketplace.sol
@@ -20,5 +20,27 @@ contract NFTMarketplace is Ownable {
     event ItemCreated(uint256 indexed itemId, address indexed creator, string uri, uint256 price);
     event ItemSold(uint256 indexed itemId, address indexed buyer, uint256 price);
 
+    function createItem(string memory uri, uint256 price) public onlyOwner {
+        _itemIds.increment();
+        uint256 itemId = _itemIds.current();
+
+        _items[itemId] = NFTItem(itemId, msg.sender, uri, price, false);
+
+        emit ItemCreated(itemId, msg.sender, uri, price);
+    }
+
+    function buyItem(uint256 itemId) public payable {
+        NFTItem storage item = _items[itemId];
+        require(item.sold == false, "Item already sold");
+        require(msg.value >= item.price, "Insufficient funds");
+
+        item.sold = true;
+        _itemsSold.increment();
+
+        emit ItemSold(itemId, msg.sender, item.price);
+
+        payable(item.creator).transfer(item.price);
+    }
+
 }
 
